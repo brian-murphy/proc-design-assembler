f2eaf62a635a5c9379e96320af29219df353fdb3
Brian Murphy <brianmurphy@gatech.edu>
2017-11-07 09:58:24 -0500

    fixed sw and lw encoding
    The opcode for lw and sw were initially swapped in the spec.
    This version corrects them.

--- code_generator/opcodes.py
+++ code_generator/opcodes.py
@@ -3,8 +3,8 @@ They are bit shifted to what will be their final position in the instructions.""
 
 ALUR = 0b1111 << 24
 ALUI = 0b1011 << 24
-LW = 0b1000 << 24
-SW = 0b1001 << 24
+LW = 0b1001 << 24
+SW = 0b1000 << 24
 CMPR = 0b1110 << 24
 CMPI = 0b1010 << 24
 BRANCH = 0 << 24
